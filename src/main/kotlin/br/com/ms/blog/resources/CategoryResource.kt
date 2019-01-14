7298fdd558e214f64918d31d363e7f083f4f2da5
Mario <mse.alvial@gmail.com>
2019-01-14 19:19:07 -0200

    Add JsonOrder to resources

--- src/main/kotlin/br/com/ms/blog/resources/CategoryResource.kt
+++ src/main/kotlin/br/com/ms/blog/resources/CategoryResource.kt
@@ -4,11 +4,12 @@ import br.com.ms.blog.controllers.CategoryController
 import br.com.ms.blog.controllers.PostController
 import br.com.ms.blog.models.Category
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import org.springframework.data.domain.Pageable.unpaged
 import org.springframework.hateoas.ResourceSupport
 import org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo
 import org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn
-
+@JsonPropertyOrder("id", "name")
 class CategoryResource(
         category: Category
 ) : ResourceSupport(){
